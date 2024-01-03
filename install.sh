2c93c498a49d6b19ad2d143f59e8c6b4cf0ee9ba
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-01-03 15:17:08 +1100

    add alacritty config

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -s ~/repos/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/repos/dotfiles/nvim ~/.config/
 ln -s ~/repos/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/repos/dotfiles/alacritty/alacritty.toml  ~/.alacritty.toml
