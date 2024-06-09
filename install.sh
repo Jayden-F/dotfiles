ae0917482d380737009822d1c6995048a2510b50
Jayden <jayden@pop-os.lan>
2024-06-09 23:44:50 +1000

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 ln -s ~/repos/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/repos/dotfiles/zsh/zprofile ~/.zprofile
 ln -s ~/repos/dotfiles/nvim ~/.config/
 ln -s ~/repos/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/repos/dotfiles/alacritty/alacritty.toml  ~/.alacritty.toml
