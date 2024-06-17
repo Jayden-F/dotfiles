fc1a8e7deefc346d0be6041bf1f20a4210cf23a2
Jayden <jaydenjfilippi@gmail.com>
2024-06-17 11:45:06 +1000

    update settings and config

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 ln -s ~/repos/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/repos/dotfiles/zsh/zprofile ~/.zprofile
 ln -s ~/repos/dotfiles/nvim ~/.config/
 ln -s ~/repos/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/repos/dotfiles/alacritty/alacritty.toml  ~/.alacritty.toml
