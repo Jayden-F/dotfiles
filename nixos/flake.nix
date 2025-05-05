8ccde4f093b2b0ce46c165d295042ea907a7f07a
jayden filippi <jaydenjfilippi@gmail.com>
2025-05-05 23:14:27 +1000

    Update my nixos config.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,16 +1,30 @@
 {
-    description = "A very basic flake";
+    description = "My Config";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        nixos-hardware.url = "github:nixos/nixos-hardware/master";
     };
 
-    outputs = { nixpkgs, ...} @ inputs :
-        {
+    outputs = { nixpkgs, nixos-hardware } :
+        let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+            };
+        in
+            {
             nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+                specialArgs = {
+                    inherit system;
+                    inherit pkgs;
+                };
                 modules = [
+                    nixos-hardware.nixosModules.framework-16-7040-amd
                     ./configuration.nix
                 ];
             };
         };
 }
+
